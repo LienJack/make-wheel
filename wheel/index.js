65b50508975bd5c199a715c58115bc2b03872984
blacksnow233 <longyuhuhao@126.com>
2019-02-06 17:56:27 +0800

    mvvm

--- wheel/index.js
+++ wheel/index.js
@@ -1,3 +1,6 @@
-// require('./promise.easy.js')
-// require('./promise.a+.js')
-require('./promise.a+.rebuild')
+
+// require('./promise/promise.easy.js')
+// require('./promise/promise.a+.js')
+require('./promise/promise.a+.rebuild')
+
+require('./mvvm/mvvm.js')
